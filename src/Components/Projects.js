0e69a862aef86b8ba62ab3c42c275c56ce6fd51a
Atita <anaguasoft@gmail.com>
2018-09-07 22:03:48 +0800

    inserted prop-types package import added type checking

--- src/Components/Projects.js
+++ src/Components/Projects.js
@@ -1,5 +1,7 @@
 import React, { Component } from 'react';
 import ProjectItem from './ProjectItem';
+import PropTypes from 'prop-types';
+
 class Projects extends Component {
     deleteProject(id){
 		this.props.onDelete(id);
@@ -23,6 +25,11 @@ class Projects extends Component {
 	      </div>
     );
   }
+}
+//type validation
+Projects.propTypes ={
+	projects: PropTypes.array,
+	onDelete: PropTypes.func
 }
 
 export default Projects;
